89d29bfa80ba0e631e696c11b2bc1c2f59247ba7
anirudh-goyal <44718345+anirudh-goyal@users.noreply.github.com>
2020-10-31 00:20:31 +0530

    Oops syntax error

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Does this deployment really work?</a>
+          Does this deployment really work?
         </h1>
 
         <p className={styles.description}>
